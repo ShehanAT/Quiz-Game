2e3dc6a034e5825eee1f80606f58796a83efa74f
Shehan Atuk <shehanatuk@gmail.com>
2019-09-21 16:36:22 -0400

    added rake task: db_reload

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -8,6 +8,15 @@ class GamesController < ApplicationController
     end 
 
     def create
+        if !session[:collection_select]
+            session[:collection_select] = params[:collection_select]
+        end 
+        getData
+        checkData
+        render "game/stage"
+    end 
+
+    def checkData 
         quizId = params[:quizId]
         answerId = params[:answerId]
         if quizId && answerId
@@ -15,16 +24,26 @@ class GamesController < ApplicationController
             answer = Answer.find(answerId)
             if quiz.answerId === answer.answerId
                 Rails.logger.info "Correct Answer Selected"
-            end 
+            else 
+                nil
+            end
         else 
-            session[:collection_select] = params[:collection_select]
-        end
-        Rails.logger.info "Session Collection_Select is #{session[:collection_select]}" 
+            nil
+        end   
+    end 
+
+    def getData 
         @collection = Collection.find(session[:collection_select])
+        total_quizzes = @collection.total_quizzes
         @quizzes = Quiz.where(collection_id: session[:collection_select])
-        @quiz = @quizzes[0]
+        random_quiz_id = rand(1..total_quizzes)
+        while random_quiz_id === params[:quizId]
+            random_quiz_id = rand(1..total_quizzes)
+        end 
+        Rails.logger.info "The random quiz id is #{random_quiz_id}"
+        @quiz = @quizzes[random_quiz_id - 1]
+        Rails.logger.info "The random quiz is #{@quiz}"
         @answers = Answer.where(quizId: @quiz.id)
-        render "game/stage"
     end 
 
 end
--- spec/quizGame_spec.rb
+++ spec/quizGame_spec.rb
@@ -8,7 +8,6 @@ RSpec.describe Quiz do
 end 
 
 RSpec.describe Answer do 
-  answer = Answer.find(1)
   it "post /level buttons should contain Question.question value for text field" do 
     Capybara.visit("/session/login")
     Capybara.fill_in 'username', :with => 'admin'
@@ -18,7 +17,7 @@ RSpec.describe Answer do
     Capybara.find("#collection_select").find(:xpath, "option[2]").select_option
     Capybara.find("#startQuizButton").click 
     sleep 0.5
-    expect(Capybara.page.first("input[type='submit']").value).to eq("#{answer.answer}")
+    expect(Capybara.page.first("input[type='submit']").value).not_to eq("")
   end 
 
   it "post /level first button should re-render to same url" do 
@@ -35,8 +34,7 @@ RSpec.describe Answer do
     expect(Capybara.page).to have_current_path("/games", ignore_query: true)
   end
 
-  it "post /level question should be displayed" do 
-    quiz = Quiz.find(1)
+  it "post /level question should be displayed" do
     Capybara.visit("/session/login")
     Capybara.fill_in 'username', :with => 'admin'
     Capybara.fill_in 'password', :with => 'admin'
@@ -45,8 +43,25 @@ RSpec.describe Answer do
     Capybara.find("#collection_select").find(:xpath, "option[2]").select_option
     Capybara.find("#startQuizButton").click 
     sleep 0.5
-    Rails.logger.info "The first question is #{quiz.question}"
-    expect(Capybara.page.first("h2").text).to eq("Question: #{quiz.question}")
+    expect(Capybara.page.first("h2").text).not_to eq("")
+  end 
+
+  it "post /level question should change on when selecting first button of quiz 1" do 
+    quiz = Quiz.find(1)
+    old_question = quiz.question
+    Capybara.visit("/session/login")
+    Capybara.fill_in 'username', :with => 'admin'
+    Capybara.fill_in 'password', :with => 'admin'
+    Capybara.find("input[value='Login']").click 
+    sleep 0.5
+    Capybara.find("#collection_select").find(:xpath, "option[2]").select_option
+    Capybara.find("#startQuizButton").click 
+    sleep 0.5
+    old_question = Capybara.page.first("h2").text
+    Capybara.page.first("input[type='submit']").click
+    sleep 0.5 
+    new_question = Capybara.page.first("h2").text 
+    expect(old_question).not_to eq(new_question)
   end 
 
 end 
