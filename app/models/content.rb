1f6df400193b3083d057832e44caeb40f92bc5e4
Shehan Atuk <shehanatuk@gmail.com>
2019-10-05 13:22:58 -0400

    fixed bugs in js toggle questions /questions/new

--- app/models/content.rb
+++ app/models/content.rb
@@ -3,13 +3,13 @@ class Content < ApplicationRecord
     def self.addQuizContent(questionArr, current_quiz_id)
         # quiz = Quiz.find(1)
         parse_questionArr = JSON.parse(questionArr)
-        for i in 0...parse_questionArr.length() do 
-            Question.create(:question => parse_questionArr["#{i}"]["question"], :quiz_id => current_quiz_id)
-            Answer.create(:answer => parse_questionArr["#{i}"]["answer1"], :quiz_id => current_quiz_id)
-            Answer.create(:answer => parse_questionArr["#{i}"]["answer2"], :quiz_id => current_quiz_id)
-            Answer.create(:answer => parse_questionArr["#{i}"]["answer3"], :quiz_id => current_quiz_id)
-            Answer.create(:answer => parse_questionArr["#{i}"]["answer4"], :quiz_id => current_quiz_id, :correct_answer => )
-        end 
+        # for i in 0...parse_questionArr.length() do 
+        #     Question.create(:question => parse_questionArr["#{i}"]["question"], :quiz_id => current_quiz_id)
+        #     Answer.create(:answer => parse_questionArr["#{i}"]["answer1"], :quiz_id => current_quiz_id)
+        #     Answer.create(:answer => parse_questionArr["#{i}"]["answer2"], :quiz_id => current_quiz_id)
+        #     Answer.create(:answer => parse_questionArr["#{i}"]["answer3"], :quiz_id => current_quiz_id)
+        #     Answer.create(:answer => parse_questionArr["#{i}"]["answer4"], :quiz_id => current_quiz_id, :correct_answer => )
+        # end 
     end 
 
     def self.getQuizContent(quiz_id)
