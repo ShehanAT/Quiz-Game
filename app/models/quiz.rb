015257f93451db66f3e67e3aa93151b394bc2b00
Shehan Atuk <shehanatuk@gmail.com>
2019-10-10 21:33:45 -0400

    refactored js for /quizzes/:id

--- app/models/quiz.rb
+++ app/models/quiz.rb
@@ -6,15 +6,16 @@ class Quiz < ApplicationRecord
 
     def self.addQuizContent(questionArr, current_quiz_id)
         parsed_questionArr = JSON.parse(questionArr)
+        get_max_question_id = "SELECT MAX(id) AS max_id FROM questions;"
+        get_max_question_sql = ActiveRecord::Base.connection.execute(get_max_question_id)
+        question_id = get_max_question_sql[0]["max_id"].to_i + 1
         for i in 0...parsed_questionArr.length() do 
-            get_max_question_id = "SELECT MAX(id) AS max_id FROM questions;"
-            get_max_question_sql = ActiveRecord::Base.connection.execute(get_max_question_id)
-            question_id = get_max_question_sql[0]["max_id"].to_i + 1
             Question.create(:question => parsed_questionArr["#{i}"]["question"], :quiz_id => current_quiz_id, :id => question_id)
             Answer.create(:answer => parsed_questionArr["#{i}"]["answer1"]["answer"], :quiz_id => current_quiz_id, :correct_answer => parsed_questionArr["#{i}"]["answer1"]["correct_answer"], :question_id => question_id)
             Answer.create(:answer => parsed_questionArr["#{i}"]["answer2"]["answer"], :quiz_id => current_quiz_id, :correct_answer => parsed_questionArr["#{i}"]["answer2"]["correct_answer"], :question_id => question_id)
             Answer.create(:answer => parsed_questionArr["#{i}"]["answer3"]["answer"], :quiz_id => current_quiz_id, :correct_answer => parsed_questionArr["#{i}"]["answer3"]["correct_answer"], :question_id => question_id)
             Answer.create(:answer => parsed_questionArr["#{i}"]["answer4"]["answer"], :quiz_id => current_quiz_id, :correct_answer => parsed_questionArr["#{i}"]["answer4"]["correct_answer"], :question_id => question_id)
+            question_id += 1
         end 
     end 
     
