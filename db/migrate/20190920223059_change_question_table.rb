1a3829690c458e7a17bb763e8a7ebd3a429cc886
Shehan Atuk <shehanatuk@gmail.com>
2019-10-28 09:22:23 -0400

    change db migration

--- db/migrate/20190920223059_change_question_table.rb
+++ db/migrate/20190920223059_change_question_table.rb
@@ -3,6 +3,6 @@ class ChangeQuestionTable < ActiveRecord::Migration[6.0]
     rename_table :questions, :answers
     rename_column :answers, :question, :answer 
     add_column :quizzes, :question, :string
-    change_column :answers, :created_at, :integer, :default => :current_timestamp
+    # change_column :answers, :created_at, :integer, :default => :current_timestamp
   end
 end
