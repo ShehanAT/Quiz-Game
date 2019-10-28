0cdc9a8091262581be2fe952529bf248ee3d52d8
Shehan Atuk <shehanatuk@gmail.com>
2019-10-28 09:25:55 -0400

    change db migration

--- db/migrate/20190923005239_rename_collection_and_quiz_tables.rb
+++ db/migrate/20190923005239_rename_collection_and_quiz_tables.rb
@@ -4,9 +4,9 @@ class RenameCollectionAndQuizTables < ActiveRecord::Migration[6.0]
     rename_table :collections, :quizzes
     rename_column :questions, :collection_id, :quiz_id
     rename_column :quizzes, :total_quizzes, :total_questions
-    change_column :questions, :created_at, :integer, :default => Time.now
-    change_column :questions, :updated_at, :integer, :default => Time.now
-    change_column :quizzes, :created_at, :integer, :default => Time.now
-    change_column :quizzes, :updated_at, :integer, :default => Time.now
+    # change_column :questions, :created_at, :integer, :default => Time.now
+    # change_column :questions, :updated_at, :integer, :default => Time.now
+    # change_column :quizzes, :created_at, :integer, :default => Time.now
+    # change_column :quizzes, :updated_at, :integer, :default => Time.now
   end
 end
